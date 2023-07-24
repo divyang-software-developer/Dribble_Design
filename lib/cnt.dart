fe7ac7d4c6aeef8d2ed057104b7218fa3b95c10a
Divyang Kanani <divyang.k@simformsolutions.com>
2023-07-24 16:43:37 +0530

    cnt

---
+++ lib/cnt.dart
@@ -0,0 +1,17 @@
+library cnt;
+
+import 'package:flutter/material.dart';
+
+class Cnt extends StatelessWidget {
+  const Cnt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
